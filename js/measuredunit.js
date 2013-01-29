a17ff6a4d0cd48de077863c347ca1c8a6f67c96c
Ivan Gromov <summer.is.gone@gmail.com>
2013-01-30 01:43:07 +0600

    Bump directory and class structure

---
+++ js/measuredunit.js
@@ -0,0 +1,77 @@
+!function ($) {
+
+    "use strict";
+    JST['measured-unit'] = '' +
+        '<div class="control-group">' +
+        '<label class="control-label" for=""></label>' +
+        '<div class="controls">' +
+            '<div class="input-append">' +
+            '<input data-value="model.Value" type="text" class="span1">' +
+            '<select data-value="model.Unit" class="span1">' +
+                '<option value="кг">кг</option>' +
+                '<option value="т">т</option>' +
+            '</select>' +
+            '</div>' +
+        '</div></div>';
+
+    SiberianEHR.MeasuredUnit = Backbone.Model.extend({
+        initialize: function(options) {
+            /* Convention: Uppercase server variables */
+            this.on('change:Unit', this.unitChanged, this);
+        },
+        unitChanged: function() {
+            var previous = this.previousAttributes(),
+                oldValue = this.get('Value');
+            this.set('Value', this.getConverter(previous.Unit, this.get('Unit'))(oldValue));
+        },
+        /**
+         * @param fromUnit
+         * @param toUnit
+         * @returns Function, that accepts value in old unit and should return value in new unit
+         */
+        getConverter: function(fromUnit, toUnit) {
+            if (fromUnit === 'кг' && toUnit === 'т') {
+                return function(value) {
+                    return value / 1000;
+                }
+            }
+            if (fromUnit === 'т' && toUnit === 'кг') {
+                return function(value) {
+                    return value * 1000;
+                }
+            }
+            // TODO: raise error if converter not found
+            // if (fromUnit === toUnit)
+            return function(value) {return value};
+        }
+    });
+
+    SiberianEHR.MeasuredUnitView = SiberianEHR.BindingView.extend({
+        templateName: 'measured-unit'
+    });
+
+
+    $.fn.measuredUnit = function (options) {
+        var settings = _.extend(options || {}, {
+            Value: 1,
+            Units: ['кг', 'т'],
+            Unit: 'т'
+        });
+
+        var model = new SiberianEHR.MeasuredUnit({
+            Value: settings.Value,
+            Unit: settings.Unit
+        });
+
+        return this.each(function () {
+            var $el = $(this),
+                view = new SiberianEHR.MeasuredUnitView({
+                    el: $el,
+                    model: model
+                });
+            view.render();
+            // TODO: handle if data already exists
+            $el.data('view', view);
+        });
+    };
+}(jQuery);
---
+++ js/siberianehr.js
@@ -0,0 +1,61 @@
+var SiberianEHR = {};
+var JST = {};
+
+
+SiberianEHR.BindingView = Backbone.View.extend({
+    initialize: function (options) {
+        // initialize me!
+    },
+
+    /**
+     * Renders HTML content into element.
+     */
+    render: function () {
+        this.$el.html(this.getTemplate()(this.getContext()));
+        this.setupBindings();
+    },
+    /**
+     * @returns Template function, ready to compile conetxt to HTML
+     */
+    getTemplate: function () {
+        return Handlebars.compile(JST[this.templateName]);
+    },
+
+    /**
+     *
+     * @returns model's JSON data
+     */
+    getContext: function () {
+        if (this.hasOwnProperty('model')) {
+            return this.model.toJSON();
+        }
+        return {};
+    },
+    /**
+     * Bind rivets.js bindings to ready DOM
+     * Save Rivets view in rivets attribute.
+     */
+    setupBindings: function () {
+        this.rivets = rivets.bind(this.el, {model: this.model});
+    }
+});
+
+rivets.configure({
+    adapter: {
+        subscribe: function (obj, keypath, callback) {
+            callback.wrapped = function (m, v) {
+                callback(v)
+            };
+            obj.on('change:' + keypath, callback.wrapped);
+        },
+        unsubscribe: function (obj, keypath, callback) {
+            obj.off('change:' + keypath, callback.wrapped);
+        },
+        read: function (obj, keypath) {
+            return obj.get(keypath);
+        },
+        publish: function (obj, keypath, value) {
+            obj.set(keypath, value);
+        }
+    }
+});
