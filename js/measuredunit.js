7a5fbbc14877ad7d428ed035067fed33f297342c
Alexander A Rakov <rakov.alexander@gmail.com>
2013-01-30 11:52:56 +0600

    options.js inserted in js\measuredunit.js

--- js/measuredunit.js
+++ js/measuredunit.js
@@ -8,41 +8,72 @@
             '<div class="input-append">' +
             '<input data-value="model.Value" type="text" class="span1">' +
             '<select data-value="model.Unit" class="span1">' +
-                '<option value="кг">кг</option>' +
-                '<option value="т">т</option>' +
+                '<option value="kg">kg</option>' +
+                '<option value="t">t</option>' +
             '</select>' +
             '</div>' +
         '</div></div>';
 
     SiberianEHR.MeasuredUnit = Backbone.Model.extend({
+        defaults: {
+            PropertyName : "",  // Name of the measured property, e.g. "temperature"
+            /** Array of possible measurement units
+             *  Unit structure:
+             *  {
+             *      measure : "",           // measurement unit, e.g. "Celsius degrees" or "Fahrenheit degrees"
+             *      minValue: undefined,    // minimal value
+             *      maxValue: undefined,    // maximal value
+             *      assumedValue: undefined,// assumed value - the value set to the control if user has not touched it
+             *      //
+             *      // Precision, in terms of number of decimal places
+             *      // Value  0 implies integer value
+             *      // Value -1 implies no rounding, the value is stored as is
+             *      //
+             *      precision: -1
+             *  }*/
+            Units : [],
+            /**
+             *
+             * @param property  {string}    Measured property name. This is the first part of the key to find a rule
+             *                              of conversion.
+             *                              When called a real function we should pass this.get('propertyName')
+             * @param fromUnit  {string}    Current measure unit name. This is the second part of the key.
+             * @param toUnit    {string}    New measure unit name. This is the last part of key.
+             * @param magnitude {number}    Current value
+             *
+             * @return {number} Value, measured in new units
+             */
+            convertValue: function (property, fromUnit, toUnit, magnitude) {
+                throw new Error('Not implemented');
+            },
+            Unit: "",           // Current measurement unit value
+            Value: undefined    // Current value
+        },
         initialize: function(options) {
             /* Convention: Uppercase server variables */
+            //Setting a property name if passed through options
+            if (typeof options.PropertyName !== "undefined")
+                this.set( { PropertyName : options.PropertyName } );
+            //Deep copy of Units
+            if (typeof options.Units !== "undefined"){
+                var units = $.extend(true, {}, options.Units);
+                this.set( { Units : units } );
+            }
+            //copying a callback function from options if set
+            if (typeof options.convertValue === "function")
+                this.set( {convertValue : options.convertValue} );
+            //Setting a unit if passed through options
+            if (typeof options.Unit !== "undefined")
+                this.set( { Unit : options.Unit } );
+            //Setting a value name if passed through options
+            if (typeof options.Value !== "undefined")
+                this.set( { Value : options.Value } );
             this.on('change:Unit', this.unitChanged, this);
         },
         unitChanged: function() {
             var previous = this.previousAttributes(),
                 oldValue = this.get('Value');
-            this.set('Value', this.getConverter(previous.Unit, this.get('Unit'))(oldValue));
-        },
-        /**
-         * @param fromUnit
-         * @param toUnit
-         * @returns Function, that accepts value in old unit and should return value in new unit
-         */
-        getConverter: function(fromUnit, toUnit) {
-            if (fromUnit === 'кг' && toUnit === 'т') {
-                return function(value) {
-                    return value / 1000;
-                }
-            }
-            if (fromUnit === 'т' && toUnit === 'кг') {
-                return function(value) {
-                    return value * 1000;
-                }
-            }
-            // TODO: raise error if converter not found
-            // if (fromUnit === toUnit)
-            return function(value) {return value};
+            this.set('Value', this.convertValue(this.get('PropertyName'), previous.Unit, this.get('Unit'), oldValue));
         }
     });
 
@@ -54,13 +85,46 @@
     $.fn.measuredUnit = function (options) {
         var settings = _.extend(options || {}, {
             Value: 1,
-            Units: ['кг', 'т'],
-            Unit: 'т'
+            Units: ['kg', 't'],
+            Unit: 't',
+            PropertyName : 'weight',
+            /**
+             * Sample measure unit converter to be exposed in measuredUnit widget
+             *
+             * @param property  {string}    Measured property name. This is the first part of the key to find a rule
+             *                              of conversion.
+             *                              When called a real function we should pass this.get('propertyName')
+             * @param fromUnit  {string}    Current measure unit name. This is the second part of the key.
+             * @param toUnit    {string}    New measure unit name. This is the last part of key.
+             * @param magnitude {number}    Current value
+             *
+             * @return {number} Value, measured in new units
+             */
+            convertValue: function (property, fromUnit, toUnit, magnitude){
+                var conversionRuleNotFoundMsg = 'Conversion rule not found';
+                switch(property){
+                    case 'weight':
+                        if (fromUnit === 'kg' && toUnit === 't'){
+                            return magnitude / 1000;
+                        }
+                        if (fromUnit === 't'  && toUnit === 'kg'){
+                            return magnitude * 1000;
+                        }
+                        throw new Error(conversionRuleNotFoundMsg);
+                        break;
+                    default:
+                        throw new Error(conversionRuleNotFoundMsg);
+                        break;
+                }
+            }
         });
 
         var model = new SiberianEHR.MeasuredUnit({
             Value: settings.Value,
-            Unit: settings.Unit
+            Unit: settings.Unit,
+            Units: settings.Units,
+            PropertyName: settings.PropertyName,
+            convertValue: settings.convertValue
         });
 
         return this.each(function () {
