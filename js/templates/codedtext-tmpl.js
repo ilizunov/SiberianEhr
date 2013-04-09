e363cb83a1e44b51f5e5370df5514bcbcf6cdf91
Alexander A Rakov <rakov.alexander@gmail.com>
2013-04-09 15:14:40 +0600

    fixed markup

--- js/templates/codedtext-tmpl.js
+++ js/templates/codedtext-tmpl.js
@@ -2,7 +2,7 @@ JST['coded-text'] = '' +
     '<div class="control-group">' +
         '<div class="controls">' +
             '<input type="hidden" class="span2 select2">' +
-            '<a class="btn btn-small remove" style="margin-top: -4px; height: 22px;"><i class="icon-remove"></i></a>' +
+            '<a class="btn remove"><i class="icon-remove"></i></a>' +
             '<span class="help-inline"></span>'+
             '<div>'+
                 '<span class="label" data-text="model.Value"></span>' +
